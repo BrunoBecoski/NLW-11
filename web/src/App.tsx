0405a96aaf7f3d5f4fb5bd83d006c2389b500674
Bruno Becoski <brunobecoski@gmail.com>
2023-01-29 16:02:46 -0300

    Web: Componente Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
+import { Header } from './components/Header';
+
 import './styles/global.css';
 
-// import { Habit } from './components/Habit';
-
-import logoImage from './assets/logo.svg';
+// import { Habit } f rom './components/Habit';
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <img src={logoImage} alt="Habits" />
+        <Header />
       </div>
     </div>
   )
