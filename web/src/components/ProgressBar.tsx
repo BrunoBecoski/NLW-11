235c394523c74b862f31715c7a3656c827860e55
Bruno Becoski <brunobecoski@gmail.com>
2023-02-10 14:56:56 -0300

    Web: Popover do detalhe do dia

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        ariar-label="Progresso de hábito completados nesse dia"
+        aria-valuenow={75}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{ width: `${props.progress}%` }}
+      />
+    </div>
+  )
+}
