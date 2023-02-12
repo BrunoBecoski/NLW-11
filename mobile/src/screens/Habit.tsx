f3d53838bba08869cc1df6863dd6faae4dc39b34
Bruno Becoski <brunobecoski@gmail.com>
2023-02-12 15:35:21 -0300

    Mobile: Utilizar componente de Checkbox

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -4,6 +4,7 @@ import dayjs from 'dayjs';
 
 import { BackButton } from '../components/BackButton';
 import { ProgressBar } from '../components/ProgressBar';
+import { Checkbox } from '../components/Checkbox';
 
 interface HabitParams {
   date: string;
@@ -34,6 +35,18 @@ export function Habit() {
         </Text>
 
         <ProgressBar progress={30} />
+
+        <View className="mt-6">
+          <Checkbox
+            title="Beber 2L de água"
+            checked={false}
+          />
+
+          <Checkbox
+            title="Caminhar"
+            checked={true}
+          />
+        </View>
       </ScrollView>
     </View>
   )
