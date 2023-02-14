2c4624a19fa434085bdf67369b0d0f1d1e12f111
Bruno Becoski <brunobecoski@gmail.com>
2023-02-14 19:36:35 -0300

    Mobile: Utilizando o clsx

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -1,4 +1,6 @@
 import { Dimensions, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import clsx from 'clsx';
+import dayjs from 'dayjs';
 
 import { generateProgressPercentage } from '../utils/generate-progress-percentage';
 
@@ -15,11 +17,21 @@ interface HabitDayProps extends TouchableOpacityProps {
 }
 
 export function HabitDay({ completed = 0, amount = 0, date, ...rest }: HabitDayProps) {
-  const amountAccomplishedPercentage = amount > 0 ? generateProgressPercentage(amount, completed) : 0
+  const completedPercentage = amount > 0 ? generateProgressPercentage(amount, completed) : 0
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today)
 
   return (
     <TouchableOpacity
-      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      className={clsx('rounded-lg border-2 m-1', {
+        'bg-zinc-900 border-zinc-800': completedPercentage === 0,
+        'bg-violet-900 border-violet-700': completedPercentage > 0 && completedPercentage < 20,
+        'bg-violet-800 border-violet-600': completedPercentage >= 20 && completedPercentage < 40,
+        'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
+        'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
+        'bg-violet-500 border-violet-400': completedPercentage >= 80,
+        'border-white border-4': isCurrentDay,
+      })}
       style={{ width: daySize, height: daySize }}
       activeOpacity={0.7}
       {...rest}
