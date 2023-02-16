b25d6e926dea24b4b8a08e8492bd15054dfeeb43
Bruno Becoski <brunobecoski@gmail.com>
2023-02-15 21:50:47 -0300

    Web: Listando hábitos possíveis do dia

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,19 +1,20 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
 import dayjs from 'dayjs';
 import clsx from 'clsx';
-import { Check } from 'phosphor-react';
 
 import { ProgressBar } from './ProgressBar';
+import { HabitsList } from './HabitsList';
 
 interface HabitDayProps {
   date: Date;
-  completed?: number
-  amount?: number
+  completed?: number;
+  amount?: number;
 }
 
 export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
   const completedPercentage = amount > 0 ? Math.round((completed / amount) * 100) : 0 
+  const today = dayjs().startOf('day').toDate()
+  const isCurrentDay = dayjs(date).isSame(today)
 
   const dayAndMonth = dayjs(date).format('DD/MM')
   const dayOfWeek = dayjs(date).format('dddd')
@@ -28,59 +29,21 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
           'bg-violet-700 border-violet-500': completedPercentage >= 40 && completedPercentage < 60,
           'bg-violet-600 border-violet-500': completedPercentage >= 60 && completedPercentage < 80,
           'bg-violet-500 border-violet-400': completedPercentage >= 80,
+          'border-white border-4': isCurrentDay,
         })}
       />
       
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
+          
           <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
 
           <ProgressBar progress={completedPercentage} />
 
-          <div className="mt-6 flex flex-col gap-3">
-            <Checkbox.Root
-              className="flex items-center gap-3 group"
-            >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-
-            <Checkbox.Root
-              className="flex items-center gap-3 group"
-            >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-
-            <Checkbox.Root
-              className="flex items-center gap-3 group"
-            >
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                <Checkbox.Indicator>
-                  <Check size={20} className="text-white" />
-                </Checkbox.Indicator>
-              </div>
-
-              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                Beber 2L de água
-              </span>
-            </Checkbox.Root>
-          </div>
+          <HabitsList
+            date={date}
+          />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
